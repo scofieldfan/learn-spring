c8edb10d34d337cf1d3aba3017c30584460d9efa
fan zhang <fan.zhang1@nio.com>
2018-10-06 19:01:49 +0800

    添加

---
+++ manong_manger/manong_manger_mapper/src/main/java/com/manong/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.manong.mapper;
+
+import com.manong.pojo.User;
+import com.manong.pojo.UserExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+}
---
+++ manong_manger/manong_manger_services/src/main/java/com/manong/service/UserService.java
@@ -0,0 +1,9 @@
+package com.manong.service;
+
+import com.manong.pojo.User;
+
+public interface UserService {
+
+    User getUserById(Integer id);
+
+}
